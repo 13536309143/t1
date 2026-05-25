e6e31ae86d9c9e8ec60fec106fc2f8bfc6429a95
Jiajun Wang <115604744+13536309143@users.noreply.github.com>
2026-05-25 16:22:12 +0800

    1

--- shaders/culling.glsl
+++ shaders/culling.glsl
@@ -1,12 +1,3 @@
-/*
-
-GPU 端的计算着色器，用于遍历 LOD 树，执行视锥体剔除和遮挡剔除，决定哪些集群最终需要被画出来
-
-主文件，包含所有 culling 模块
-
-*/
-
-// 包含所有 culling 模块
 #include "culling_constants.inc"
 #include "culling_frustum.inc"
 #include "culling_hiz.inc"
